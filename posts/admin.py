208f578dfb316bdffcc9eb4b81656dd6b6b73ae9
manabreakmura <174273321+manabreakmura@users.noreply.github.com>
2024-09-12 18:35:43 +0300

    Update post model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+
+admin.site.register(Post, PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=32)
+    body = models.TextField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ("-updated_at",)
+
+    def __str__(self) -> str:
+        return f"{self.user.username}: {self.title} ({self.created_at})"
