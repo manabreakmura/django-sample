1a0502fa1263a3483558785f2de2874441405efb
manabreakmura <174273321+manabreakmura@users.noreply.github.com>
2024-09-12 18:10:25 +0300

    Add users tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from users.models import User
+
+
+@pytest.fixture
+def create_user_request(client):
+    return client.post(
+        "/users/", {"username": "test", "email": "test@user.com", "password": "test"}
+    )
+
+
+@pytest.mark.django_db
+def test_should_return_all_users(client, create_user_request):
+    response = client.get("/users/")
+    assert response.status_code == 200
+    assert User.objects.first().username == "test"
+
+
+@pytest.mark.django_db
+def test_should_create_user(client, create_user_request):
+    assert create_user_request.status_code == 201
+    assert ("username", "test") in create_user_request.data.items()
+
+
+@pytest.mark.django_db
+def test_should_return_user_by_id(client, create_user_request):
+    user = create_user_request.data
+    response = client.get(f"/users/{user['id']}/")
+    assert response.status_code == 403
+
+    client.login(username="test", password="test")
+    response = client.get(f"/users/{user['id']}/")
+    assert response.status_code == 200
+    assert ("username", "test") in response.data.items()
+
+
+@pytest.mark.django_db
+def test_should_delete_user(client, create_user_request):
+    user = create_user_request.data
+    response = client.delete(f"/users/{user['id']}/")
+    assert response.status_code == 403
+
+    client.login(username="test", password="test")
+    response = client.delete(f"/users/{user['id']}/")
+    assert response.status_code == 204
+
+    assert User.objects.filter(id=user["id"]).exists() == False
