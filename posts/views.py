6b17d49cbffc9c55759356e94a575b94ebb2fd21
manabreakmura <174273321+manabreakmura@users.noreply.github.com>
2024-09-12 20:29:25 +0300

    Update posts views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import filters, mixins, status, viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from posts.models import Post
+from posts.permissions import PostPermission
+from posts.serializers import PostSerializer
+
+
+class PostViewSet(
+    viewsets.GenericViewSet,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (PostPermission,)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title"]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data["user"] = request.user
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
